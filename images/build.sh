e20e423c8009fd6e8b4ee408930757151d9a993f
Ubuntu <aguirre@vm-docker.g02rrjrztwgenmjbdmslq0xzrd.parx.internal.cloudapp.net>
2022-02-04 13:48:44 +0000

    configuration fichier

--- images/build.sh
+++ images/build.sh
@@ -6,26 +6,26 @@ JUPYTERLAB_VERSION="2.1.5"
 
 # -- Building the Images
 
-docker images \
-  -f cluster-base.Dockerfile \
-  -t cluster-base .
+docker build \
+  -f docker/cluster-base/Dockerfile \
+  -t cluster-base:latest .
 
-docker images \
+docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg hadoop_version="${HADOOP_VERSION}" \
-  -f spark-base.Dockerfile \
-  -t spark-base .
+  -f docker/spark-base/Dockerfile \
+  -t spark-base:${SPARK_VERSION} .
 
-docker images \
-  -f spark-master.Dockerfile \
-  -t spark-master .
+docker build \
+  -f docker/spark-master/Dockerfile \
+  -t spark-master:${SPARK_VERSION} .
 
-docker images \
-  -f spark-worker.Dockerfile \
-  -t spark-worker .
+docker build \
+  -f docker/spark-worker/Dockerfile \
+  -t spark-worker:${SPARK_VERSION} .
 
-docker images \
+docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
-  -f jupyterlab.Dockerfile \
-  -t jupyterlab .
+  -f docker/jupyterlab/Dockerfile \
+  -t jupyterlab:${JUPYTERLAB_VERSION}-spark-${SPARK_VERSION} .
